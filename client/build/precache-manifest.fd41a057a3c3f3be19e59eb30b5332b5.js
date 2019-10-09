76678d5e2d0c361af6d94767268f3e51c7ea2b6d
Keighley McFarland <mcfarland.ke@gmail.com>
2019-10-09 16:49:25 +0200

    build added for deploy

---
+++ client/build/precache-manifest.fd41a057a3c3f3be19e59eb30b5332b5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55dee5305696476f82d4090157719ab5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27c615215d255b4222ca",
+    "url": "/static/css/main.29d9bef7.chunk.css"
+  },
+  {
+    "revision": "e7cdda269cf881d3941f",
+    "url": "/static/js/2.4ce00884.chunk.js"
+  },
+  {
+    "revision": "27c615215d255b4222ca",
+    "url": "/static/js/main.586bffe0.chunk.js"
+  },
+  {
+    "revision": "7f45875b99a5ef02142c",
+    "url": "/static/js/runtime-main.c2763ce9.js"
+  }
+]);
