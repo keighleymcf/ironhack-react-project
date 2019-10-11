2412bf253bedfdca4a6f750b07d62cba8051c891
Keighley McFarland <mcfarland.ke@gmail.com>
2019-10-11 10:01:46 +0200

    fix skew

---
+++ client/build/precache-manifest.f127f5f0639043e95661fe5a86431738.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a402a3423ed69608c2340635f94e99e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f97fd8b9691af1ee36e3",
+    "url": "/static/css/main.1cc5a5ea.chunk.css"
+  },
+  {
+    "revision": "62b9493656cee6c20bc7",
+    "url": "/static/js/2.af9e0a80.chunk.js"
+  },
+  {
+    "revision": "f97fd8b9691af1ee36e3",
+    "url": "/static/js/main.cec4dabb.chunk.js"
+  },
+  {
+    "revision": "7f45875b99a5ef02142c",
+    "url": "/static/js/runtime-main.c2763ce9.js"
+  }
+]);
